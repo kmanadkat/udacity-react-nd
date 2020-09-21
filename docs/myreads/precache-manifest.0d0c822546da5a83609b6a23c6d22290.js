e1b790790420fadbc14174021310df62a3b1b300
Krupesh Anadkat <krupesh.anadkat@xebia.com>
2020-09-21 13:27:26 +0530

    build push

---
+++ docs/myreads/precache-manifest.0d0c822546da5a83609b6a23c6d22290.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85e47fd1572900b4ad040552420eb68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5cad0d70ad2d61eb1f1",
+    "url": "/static/css/main.4a08670e.chunk.css"
+  },
+  {
+    "revision": "ee617927ad2c74897cc4",
+    "url": "/static/js/2.6e13d874.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6e13d874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5cad0d70ad2d61eb1f1",
+    "url": "/static/js/main.bec1eb15.chunk.js"
+  },
+  {
+    "revision": "6f08544fabe75b67c30f",
+    "url": "/static/js/runtime-main.a7c61e3d.js"
+  },
+  {
+    "revision": "34bf196807ba8d71c0157052e8eabd04",
+    "url": "/static/media/logo.34bf1968.png"
+  },
+  {
+    "revision": "b798ff64743fd84b3efe451b6e7a5463",
+    "url": "/static/media/noThumbnail.b798ff64.png"
+  }
+]);
